3ac1ee2f5275734ae851b95e596d77c594013fe0
Peter Broman <jp.broman@telia.com>
2026-05-09 09:48:22 +0200

    Adding more func and tests

--- GarageExercise.Tests/UnitTest1.cs
+++ GarageExercise.Tests/UnitTest1.cs
@@ -14,7 +14,7 @@ public class UnitTest1
     {
         Garage garage = new Garage(5);
         Vehicle vehicle = new Vehicle("Toyota", "Red", TypeE.Car, "ABC123");
-        garage.AddVehicle(vehicle);
+        Assert.True(garage.AddVehicle(vehicle));
         Assert.Equal(1, garage.numberOfVehicles);
     }
 
@@ -27,5 +27,19 @@ public class UnitTest1
         Assert.Equal(1, garage.numberOfVehicles);
         garage.RemoveVehicle(vehicle);
         Assert.Equal(0, garage.numberOfVehicles);
+    }
+
+    [Fact]
+    public void TestAddTooManyVehicles()
+    {
+        Garage garage = new Garage(2);
+        Vehicle vehicle1 = new Vehicle("Toyota", "Red", TypeE.Car, "ABC123");
+        Vehicle vehicle2 = new Vehicle("Honda", "Blue", TypeE.Motorcycle, "XYZ789");
+        Vehicle vehicle3 = new Vehicle("Ford", "Green", TypeE.Truck, "DEF456");
+        garage.AddVehicle(vehicle1);
+        garage.AddVehicle(vehicle2);
+        Assert.False(garage.AddVehicle(vehicle3));
+        garage.RemoveVehicle(vehicle1);
+        Assert.True(garage.AddVehicle(vehicle3));
     }
 }
--- GarageExercise/Garage.cs
+++ GarageExercise/Garage.cs
@@ -3,17 +3,18 @@ using System.Collections;
 namespace GarageExercise;
 public class Garage
 {
-    private Vehicle[] Vehicles;
+    private Vehicle?[] Vehicles;
     public int numberOfVehicles { get ; private set; }
     public Garage(int availableSpace)
     {
-        Vehicles = new Vehicle[availableSpace];
+        Vehicles = new Vehicle?[availableSpace];
         numberOfVehicles = 0;
     }
 
-    public void AddVehicle(Vehicle vehicle)
+    public bool AddVehicle(Vehicle vehicle)
     {
         // check if there is room for this vehicle. If not, print a message and return.
+/*
         if (numberOfVehicles < Vehicles.Length)
         {
             Vehicles[numberOfVehicles] = vehicle;
@@ -24,10 +25,38 @@ public class Garage
             Console.WriteLine("No room for this vehicle in the garage.");
         }
     }
-
+        for (int i = 0; i < Vehicles.Length; i++)
+        {
+            if (Vehicles[i] == null)
+            {
+                Vehicles[i] = vehicle;
+                numberOfVehicles++;
+                return;
+            }
+        }
+*/
+        int i = Array.IndexOf(Vehicles, null);
+        if (i >= 0)        {
+            Vehicles[i] = vehicle;
+            numberOfVehicles++;
+            return true;
+        }
+        else {       
+            Console.WriteLine("No room for this vehicle in the garage.");
+        }
+        return false;
+    }
     public void RemoveVehicle(Vehicle vehicle)
     {
-        // TODO. Implement this method.
+        int i = Array.IndexOf(Vehicles, vehicle);
+        if (i >= 0)        {
+            Vehicles[i] = null;
+            numberOfVehicles--;
+        }
+        else
+        {
+            Console.WriteLine("Vehicle not found in the garage.");
+        }
     }
 
     public void ListVehicles()
